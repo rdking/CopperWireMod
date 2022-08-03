c9e2f11371d26d083557f4c3f3aaddd4fda8737b
Ranando King <kingmph@gmail.com>
2022-08-03 09:35:09 -0400

    Corrected a few constants

--- src/main/java/net/apltd/copperwiremod/CopperWireModClient.java
+++ src/main/java/net/apltd/copperwiremod/CopperWireModClient.java
@@ -1,5 +1,6 @@
 package net.apltd.copperwiremod;
 
+import net.apltd.copperwiremod.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
@@ -7,6 +8,6 @@ import net.minecraft.client.render.RenderLayer;
 public class CopperWireModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperWireMod.COPPERPOWERMETER, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COPPER_POWERMETER, RenderLayer.getTranslucent());
     }
 }
--- src/main/java/net/apltd/copperwiremod/blockentity/ModBlockEntities.java
+++ src/main/java/net/apltd/copperwiremod/blockentity/ModBlockEntities.java
@@ -1,17 +1,10 @@
 package net.apltd.copperwiremod.blockentity;
 
-import net.apltd.copperwiremod.CopperWireMod;
-import net.apltd.copperwiremod.block.CopperWire;
 import net.apltd.copperwiremod.block.ModBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.registry.Registry;
 
-import java.util.function.Supplier;
-
-@SuppressWarnings({"unchecked", "rawtypes"})
 public class ModBlockEntities {
 
     public static BlockEntityType<CopperWireEntity> COPPERWIRE_ENTITY;
@@ -19,7 +12,7 @@ public class ModBlockEntities {
     public static void registerBlockEntities() {
         COPPERWIRE_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE,
                 CopperWireEntity.COPPERWIRE_ENTITYNAME,
-                FabricBlockEntityTypeBuilder.create(CopperWireEntity::new, CopperWireMod.COPPERWIRE)
+                FabricBlockEntityTypeBuilder.create(CopperWireEntity::new, ModBlocks.COPPER_WIRE)
                         .build(null));
     }
 }
